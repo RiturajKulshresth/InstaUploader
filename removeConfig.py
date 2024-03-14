59ead9b0d4bf5e048fb155b3b77f896b29345a34
Rituraj Kulshresth <idohaveemailid@gmail.com>
2024-03-15 05:07:44 +0530

    added gui

--- removeConfig.py
+++ removeConfig.py
@@ -3,6 +3,7 @@ import shutil
 
 def main():
     folder_path = "./config"
+    Cred_path = "./Creds.py"
     if os.path.exists(folder_path):
         try:
             shutil.rmtree(folder_path)  # Deletes the folder and its contents
@@ -11,6 +12,15 @@ def main():
             print(f"Error: {e.strerror}")
     else:
         print(f"Folder '{folder_path}' does not exist.")
+
+    if os.path.exists(Cred_path):
+        try:
+            os.remove(Cred_path)  # Deletes the folder and its contents
+            print(f"File '{Cred_path}' deleted successfully.")
+        except OSError as e:
+            print(f"Error: {e.strerror}")
+    else:
+        print(f"File '{Cred_path}' does not exist.")
 
 if __name__ == "__main__":
         main()
